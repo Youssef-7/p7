78a6b6ceaee73f84990919fd4913a16ce3e1abc9
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2021-11-24 13:38:26 +0100

    Add files via upload

---
+++ frontend/src/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <About/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import About from '@/components/pageAccueil.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    About
+  }
+}
+</script>
---
+++ frontend/src/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <formConnexion/>
+</template>
+
+<script>
+// @ is an alias to /src
+import formConnexion from '@/components/formConnexion.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    formConnexion
+  }
+}
+</script>
