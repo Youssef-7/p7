94591eb006a9af94f59faf4ebd0dba960670c39c
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2021-11-24 13:39:19 +0100

    Add files via upload

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -3,6 +3,7 @@ const db = require('../config/database');
 // Get  post initiaux (debut de conversation)
 
 exports.insertUser = (data, result) => {
+    console.log("INSERT INTO usagers SET u_pseudo = ?, u_email = ?, u_pwd = ? " + data.u_pseudo, data.u_email, data.u_pwd);
      db.query(
     //UPDATE product SET product_name = ?, product_price = ? WHERE product_id = ?", [data.product_name, data.product_price, id], (err, results) => { 
       // "INSERT INTO usagers (u_pseudo, u_email, u_pwd,) VALUES (?,?,?)"
@@ -18,7 +19,9 @@ exports.insertUser = (data, result) => {
 
 // Get Single post
 exports.getUsers = (data, result) => {
-      db.query( "SELECT * FROM usagers WHERE u_email = ?;", [data.u_email], (err, results) => {             
+                console.log("models/user_model: data " + data.u_email);
+            console.log("SELECT * FROM usagers WHERE u_email = " + data.u_email);
+      db.query( "SELECT * FROM usagers WHERE u_email = ?;", [u_email], (err, results) => {             
         if(err) {
             console.log(err);
             result(err, null);
